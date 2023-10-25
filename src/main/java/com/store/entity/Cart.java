ec4b6482026a253301a2b52bcdd7c75f882b7011
Pedro Lorenzo <pedrolorenzolabrador@gmail.com>
2023-10-25 21:37:06 +0200

    Added method to cart to check if its expired and toString with date format

--- src/main/java/com/store/entity/Cart.java
+++ src/main/java/com/store/entity/Cart.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
@@ -15,6 +15,9 @@ import java.util.List;
 @NoArgsConstructor
 public class Cart {
 
+    //That are 10 minute in milliseconds
+    public static final Long EXPIRATION_TIME = 600000L;
+
     public Cart(List<Product> products,Date lastModified) {
         this.products= products;
         this.lastModified= lastModified;
@@ -23,9 +26,24 @@ public class Cart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToMany
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "cart_product",
+            joinColumns = @JoinColumn(name = "cart_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
     private List<Product> products;
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModified;
+
+    @Override
+    public String toString() {
+        //Modified toString() to get most usable date format
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = formatter.format(lastModified);
+
+        return String.format("Cart(id=%d, products=%s, lastModified=%s)", id, products, formattedDate);
+    }
+    public boolean isExpired() {
+        return new Date().getTime() - getLastModified().getTime() > EXPIRATION_TIME;
+    }
 }
